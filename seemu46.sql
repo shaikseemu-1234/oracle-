c4d7c913d84c17a0e840208e41ab9162d036dde4
shaikseemu-1234 <shaikseemu123@gmail.com>
2025-03-13 20:10:09 +0530

    Update seemu46.sql

--- seemu46.sql
+++ seemu46.sql
@@ -1,32 +1,32 @@
-select*from hr.job_history
-select sysdate from dual
-select systimestamp from dual
-select sysdate from hr.job_history
-select systimestamp from  hr.job_history
-select current_date from hr.job_history
-select current_timestamp from hr.job_history
-select dbtimezone from hr.job_history
-select sessiontimezone from  hr.job_history
-select sysdate ,sysdate+8  as nextweek from  hr.job_history
-select sysdate ,sysdate+8 from  hr.job_history
-select sysdate ,sysdate-8 as previousweek from  hr.job_history
-select sysdate ,sysdate+8 from  hr.job_history
+select*from sh.sales
+select sysdate from sh.sales
+select systimestamp from sh.sales
+select sysdate from sh.sales
+select systimestamp from sh.sales
+select current_date from sh.sales 
+select current_timestamp from sh.sales
+select dbtimezone from sh.sales
+select sessiontimezone from  sh.sales
+select sysdate ,sysdate+8  as nextweek from  sh.sales
+select sysdate ,sysdate+8 from sh.sales
+select sysdate ,sysdate-8 as previousweek from  sh.sales
+select sysdate ,sysdate+8 from sh.sales
 select systimestamp,systimestamp+interval'4' hour from dual
-select systimestamp,systimestamp+interval'4' hour from hr.job_history
-select systimestamp,systimestamp+interval'4' minute from hr.job_history
-select systimestamp,systimestamp+interval'4' day from hr.job_history
-select systimestamp,systimestamp+interval'2' month from hr.job_history
-select systimestamp,systimestamp+interval'2' year from hr.job_history
-select systimestamp+interval '2' day+interval'4' hour from hr.job_history
-select systimestamp+interval'5' day+interval '3'hour from hr.job_history
-select start_date ,extract( year from start_date)year from hr.job_history
-select start_date,extract(month from start_date) month from hr.job_history
-select start_date ,extract( year from start_date)year ,extract(month from start_date)as month ,
-extract(day from start_date)as day from hr.job_history
-select add_months(sysdate,6) from hr.job_history
-select months_between (start_date,sysdate)from hr.job_history
-select start_date ,last_day(start_date) from hr.job_history
-select *from hr.job_history
-select job_id from hr.job_history
+select systimestamp,systimestamp+interval'4' hour from sh.sales
+select systimestamp,systimestamp+interval'4' minute from sh.sales
+select systimestamp,systimestamp+interval'4' day from sh.sales
+select systimestamp,systimestamp+interval'2' month from sh.sales
+select systimestamp,systimestamp+interval'2' year from sh.sales
+select systimestamp+interval '2' day+interval'4' hour from sh.sales
+select systimestamp+interval'5' day+interval '3'hour from sh.sales
+select time_id ,extract( year from time_id)year from sh.sales
+select time_id,extract(month from time_id) month from sh.sales
+  select time_id ,extract( year from time_id)year ,extract(month from time_id)as month ,
+extract(day from time_id)as day from sh.sales
+select add_months(sysdate,6) from sh.sales
+select months_between (time_id,sysdate)from sh.sales
+select time_id ,last_day(time_id) from sh.sales
+select *from sh.sales
+select prod_id from sh.sales
 -- null function
-select null from hr.job_history
+select null from sh.sales
